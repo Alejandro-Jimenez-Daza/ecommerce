1b4f7ace4e3a60a26fb2bfd8019bf74fdb3800e0
Alejandro-Jimenez-Daza <alejodaza061@gmail.com>
2026-02-24 13:05:39 -0500

    se comienza a maquetar el dashboard de la pagina wbe

--- index.php
+++ index.php
@@ -6,41 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="resources/css/index.css">
 </head>
 
 <body>
 
     <!-- ESTE DOCUMENTO CONTIENE EL FORMULARIO DE LOGIN A LA PAGINA WEB DE ECOMMERCE. -->
 
-    <div class="container">
+    <div class="d-flex justify-content-center align-items-center vh-100" id="contenedor-login">
 
-        <div class="bg-info">
+        <div class="container">
 
-            <!-- formulario de ingreso de sesion a la plataforma -->
-            <form action="controller/login/loginController.php" method="post">
+            <div class="">
 
-                <!-- entrada del correo -->
-                <div id="email">
-                    <label for="">correo electronico</label> <br>
-                    <input type="email" placeholder="ejemplo@gmail.com" name="email" required>
-                </div>
+                <!-- formulario de ingreso de sesion a la plataforma -->
+                <form action="controller/login/loginController.php" method="post">
 
-                <!-- entrada de la clave -->
-                <div id="password">
-                    <label for="" class="mt-3">contrasena</label> <br>
-                    <input type="password" placeholder="●●●●●●●●" name="pass" required>
-                </div>
+                    <!-- entrada del correo -->
+                    <div id="email">
+                        <label for="">correo electronico</label> <br>
+                        <input type="email" placeholder="ejemplo@gmail.com" name="email" required id="input-login">
+                    </div>
 
-                <button class="btn btn-success mt-3" type="submit">Entrar</button><br>
+                    <!-- entrada de la clave -->
+                    <div id="password">
+                        <label for="" class="mt-3">contrasena</label> <br>
+                        <input type="password" placeholder="●●●●●●●●" name="pass" required id="input-login">
+                    </div>
 
-                <p>NO tienes cuenta? <a href="view/crearCuenta.php">cree una</a></p>
+                    <button class="btn btn-success mt-3" type="submit">Entrar</button><br>
+
+                    <p>NO tienes cuenta? <a href="view/crearCuenta.php">cree una</a></p>
 
 
-            </form>
+                </form>
+
+            </div>
 
         </div>
-
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
--- view/dashboard.php
+++ view/dashboard.php
@@ -5,10 +5,76 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../resources/css/dashboard.css">
 </head>
 
 <body>
-    <h1>bienvenido al sistema</h1>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid pb-3" id="color-nav">
+            <a class="navbar-brand text-white" href="#">Ecommerce</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Admin panel
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Productos</a></li>
+                            <li><a class="dropdown-item" href="#">Usuarios</a></li>
+
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Contacto</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            User name
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Cerrar sesion</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- contenido y tarjetas para los productos, aca ciclo el bucle de los productos -->
+    <div class="bg-info">
+        <h2 class="text-center">productos recientes</h2>
+
+        <div class="row">
+
+            <div class="card" style="width: 18rem;">
+                <img src="../resources/static/bix.jpg" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="../resources/static/psu.webp" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
 </html>
