269adf02befc21793d158374572b180af906b7f4
Alejandro-Jimenez-Daza <alejodaza061@gmail.com>
2026-02-27 04:18:54 -0500

    ruta relativa del css en el nav corregida con BASE_URL

--- view/navbar.php
+++ view/navbar.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../config/config.php';
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="../resources/css/nav.css">
+<link rel="stylesheet" href="<?= BASE_URL ?>/resources/css/nav.css">
 
 <nav class="navbar navbar-expand-lg navbar-custom">
     <div class="container">
